f5b2f46544706cc3542127f7c35e170bf6dd2c7f
Rodrigo Singh <rodrigo0singh@gmail.com>
2014-03-11 20:52:57 -0400

    Added more comments and commented out console logging.

--- js/reddit.js
+++ js/reddit.js
@@ -3,6 +3,9 @@
 // After holds the id of the bottom most link.
 var redditTracker = {jsonurl:"http://www.reddit.com/r/news+worldnews+truenews/new/.json"}
 
+/*
+ * Processes the older links data
+ */
 var getAfterLinks = function(json) {
 	var links = json.data.children;
 	var html = createHtmlFromLinks(links);
@@ -10,6 +13,9 @@ var getAfterLinks = function(json) {
 	$('#content').append(html);
 };
 
+/*
+ * Handles new link data
+ */
 var getBeforeLinks = function(json) {
 	var links = json.data.children;
 	// Check if there are links. If there are links, then update the before field
@@ -30,20 +36,29 @@ var getBeforeLinks = function(json) {
 }
 
 $( document ).ready(function() {
+	// Get the JSON with an ajax call
 	$.getJSON(redditTracker.jsonurl + "?limit=10", function(json) {
+		// This is the data
 		var links = json.data.children;
+		// Initialize the before field with this data
 		redditTracker.before = links[0].data.name;
 		redditTracker.currentTop10 = links;
+		// Create HTML markup from the links data
 		var html = createHtmlFromLinks(links);
 		//console.log(json.data);
 		//console.log(links);
+		// Initialize the after field with this data
 		redditTracker.after = json.data.after;
+		// Append the HTML to the page.
 		$('#content').append(html);
+		// Clear the data in the queue
 		redditTracker.linksQueue = new Array();
+		// Create the buttons that will be used to display new data when it comes in
 		var new_data = '<input type="button" class="new-data" value="' + redditTracker.linksQueue.length + ' new data sets available">'
 		$('#top-holder').append(new_data);
 		$('#bottom-holder').append(new_data);
 		$('.new-data').css('visibility', 'hidden');
+		// Event handler for the buttons
 		$('.new-data').click(function() {
 			var html = createHtmlFromLinks(redditTracker.linksQueue);
 			$('#content').prepend(html);
@@ -54,6 +69,9 @@ $( document ).ready(function() {
 	});
 });
 
+/*
+ * Repeatedly checks for new data from the API
+ */
 setInterval(function() {
 	var json10beforeurl = redditTracker.jsonurl + "?before=" + redditTracker.before + "&limit=100";
 	$.getJSON(json10beforeurl, getBeforeLinks);
@@ -67,7 +85,7 @@ function createHtmlFromLinks(links) {
 	//console.log(links.length);
 	for(var i = 0; i < links.length; ++i) {
 		var link = links[i].data;
-		console.log(link.id);
+		//console.log(link.id);
 		var title = link.title;
 		var thumb = link.thumbnail;
 		var url = "http://www.reddit.com"+link.permalink;
@@ -82,6 +100,9 @@ function createHtmlFromLinks(links) {
 	return html;
 }
 
+/*
+ * Event handler for the button that is used for showing 10 more older links
+ */
 function morelinks() {
 	var json10afterurl = redditTracker.jsonurl + "?after=" + redditTracker.after + "&limit=10";
 	$.getJSON(json10afterurl, getAfterLinks);
@@ -89,6 +110,7 @@ function morelinks() {
 
 /*
  * Taken from http://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+ * Converts the UNIX timestamp taken from the data into a readable date.
  */
 function timeConverter(UNIX_timestamp){
  var a = new Date(UNIX_timestamp*1000);
